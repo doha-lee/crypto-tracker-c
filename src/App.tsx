717759e5ea656482d5cd307e719e2858ab843b4c
Doha <hi.dohalee@gmail.com>
2022-11-12 04:09:24 +0900

    challenge 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import React, { useState } from "react";
+import Router from "./Router";
+import GlobalStyle from "./styles/GlobalStyle";
+import { darkTheme, lightTheme } from "./styles/theme";
+import { BiSun, BiMoon, BiArrowBack } from "react-icons/bi";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+
+const Header = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin: 0 auto;
+	max-width: 480px;
+	padding: 20px;
+	border-radius: 15px;
+	font-size: 32px;
+`;
+
+const Icon = styled.div``;
+
+const Button = styled.div``;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [darkMode, setDarkMode] = useState(true);
+	const toggleMode = () => {
+		setDarkMode((prev) => !prev);
+	};
+
+	let history = useHistory();
+	const handleBack = () => {
+		history.goBack();
+	};
+
+	return (
+		<>
+			<ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+				<GlobalStyle />
+				<Header>
+					<Icon onClick={toggleMode}>
+						{darkMode ? <BiMoon /> : <BiSun />}
+					</Icon>
+					<Button onClick={handleBack}>
+						<BiArrowBack />
+					</Button>
+				</Header>
+				<Router />
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default App;
